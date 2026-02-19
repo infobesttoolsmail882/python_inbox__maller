27fbd0a5d4b53fc6870605e2471290720da7f1ab
infobesttoolsmail882 <info@besttoolsmail.online>
2026-02-19 14:17:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,4 @@
-# ===== MAIL CONFIGURATION =====
+import os
 
-MAX_EMAILS_PER_BATCH = 100        # Ek batch me max emails
-DELAY_BETWEEN_EMAILS = 0.3        # 0.3 sec delay (fast but safe)
-MAX_SUBJECT_LENGTH = 150
-MAX_MESSAGE_LENGTH = 5000
-
-SMTP_SERVER = "smtp.gmail.com"
-SMTP_PORT = 587
+EMAIL = os.environ.get("EMAIL")
+PASSWORD = os.environ.get("PASSWORD")
