878b7629b7ee971a56d72b924dde191a2c4d75dc
infobesttoolsmail882 <info@besttoolsmail.online>
2026-02-19 14:07:36 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# ===== MAIL CONFIGURATION =====
+
+MAX_EMAILS_PER_BATCH = 100        # Ek batch me max emails
+DELAY_BETWEEN_EMAILS = 0.3        # 0.3 sec delay (fast but safe)
+MAX_SUBJECT_LENGTH = 150
+MAX_MESSAGE_LENGTH = 5000
+
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
